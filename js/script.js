13892d9216b069f59ad12c534fafcb5efeac9510
svetlana18055@yandex.ru <svetlana18055@yandex.ru>
2021-09-30 23:10:03 +0300

    alert и console.log в файле script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-'use strict'
+"use strict"
+
+let title;
+ let screens = 'Hello, world!';
+ let screenPrice;
+ let rollback;
+ let fullPrice;
+ let adaptive;
+
+
+ function showAlert(){
+     title = alert(screens);
+     console.log(screens);
+ }
+ showAlert();
