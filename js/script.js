2599b2192f70a860e869623378ae1039e02786e8
svetlana18055@yandex.ru <svetlana18055@yandex.ru>
2021-10-02 16:15:42 +0300

    урок 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 "use strict"
 
-let title;
- let screens = 'Hello, world!';
- let screenPrice;
- let rollback;
- let fullPrice;
- let adaptive;
+let title ='Урок2'
+console.log(typeof(title));
+ let screens = 'Простые, Сложные, Интерактивные'
+ console.log(screens.length)
+ let screenPrice = 5200
+ let rollback = 25
+ let fullPrice = 120000
+ console.log(typeof(fullPrice))
+ let adaptive = true
+ console.log(typeof(adaptive))
+
+ console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей/ долларов/гривен/юани')
+ console.log('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей/ долларов/гривен/юани')
+
+ screens.toLowerCase;
+ console.log(screens.split(', '))
 
 
- function showAlert(){
-     title = alert(screens);
-     console.log(screens);
- }
- showAlert();
+ fullPrice += fullPrice * (rollback/100)
+ console.log(fullPrice)
+
+
+
+
+
+ 
