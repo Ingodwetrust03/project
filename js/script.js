969d4e4e8d2735993986eaaf1cab1d6f8c40781d
svetlana18055@yandex.ru <svetlana18055@yandex.ru>
2021-10-07 20:46:11 +0300

    урок 5

--- js/script.js
+++ js/script.js
@@ -1,39 +1,67 @@
 "use strict"
 
-let title =prompt('Как называется ваш проект?')
- let screens = prompt('Какие типы экранов нужно разработать?')
- let screenPrice = +prompt('Сколько будет стоить данная работа?')
- let rollback = 25
- let adaptive = Boolean(prompt('Нужен ли адаптив на сайте?'))
-let service1 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice1 = +prompt('Сколько это будет стоить?')
-let service2 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice2 = +prompt('Сколько это будет стоить?')
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
 
+let rollback = 25
 
+let title, screens, screenPrice, servicePercentPrice, allServicePrices, service1, servicePrice1, service2, servicePrice2,adaptive, fullPrice
 
-const allServicePrices = function getAllServicePrices(){
-    return servicePrice1 + servicePrice2
-    }
+ 
 
-
-function getFullPrice(){
-    return screenPrice + allServicePrices()
+const isNumber = function(num){
+    return !isNaN(parseFloat(num)) && isFinite(num)
 }
 
-fullPrice = getFullPrice()
+ 
+const showTypeOf = function(variable){
+    console.log(variable, typeof(variable))
+}
 
+ 
+
+const questions = function(){
+    let getScreenPrice = 0
+
+title =prompt('Как называется ваш проект?', 'проект')
+screens = prompt('Какие типы экранов нужно разработать?', 'сложные')
+
+do {
+    screenPrice = +prompt('Сколько будет стоить данная работа?')
+} while(!isNumber(screenPrice))
+ 
+getScreenPrice = screenPrice
+
+adaptive = confirm('Нужен ли адаптив на сайте?')
+ }
+
+ 
+
+ const getAllServicePrices = function (){
+let sum =0
+    for (let i=0; i<2; i++){
+        if (i===0){
+            service1 = prompt('Какой дополнительный тип услуги нужен?') 
+        } else if(i===1) {
+            service2 = prompt('Какой дополнительный тип услуги нужен?')  
+        }
+
+do {
+    sum = +prompt('Сколько будет стоить данная работа?')
+    console.log(sum)
+} while(!isNumber(screenPrice))
+    sum += +sum
+    }
+    return sum
+}
+
+    
 
 
 const getTitle = function(){
     let bigFirstLetter, titleArrayToString
     let titleToLow =  title.toLowerCase()
-    let titleArray = titleToLow.split('')
- 
+    let titleArray = titleToLow.split("")
 
-    
-    function makeBigFirstLetter(){
+function makeBigFirstLetter(){
         bigFirstLetter = titleArray[0].toUpperCase()
         titleArray.shift();
         titleArray.unshift(bigFirstLetter)
@@ -47,19 +75,23 @@ const getTitle = function(){
     } else {
         makeBigFirstLetter()
     } 
-
-    
 }
 
+ 
 
-const servicePercentPrice = function getServicePercentPrices(){
+const getFullPrice = function(){
+    return screenPrice + allServicePrices
+}
+ 
+
+const getServicePercentPrices = function(){
         return parseFloat(fullPrice - fullPrice * (rollback/100))
-    
 }
 
+ 
 
-
-switch (true){
+function showMessage(){
+    switch (true){
     case fullPrice > 30000:
         console.log('Даем скидку в 10%')
         break;
@@ -67,7 +99,7 @@ switch (true){
         console.log('Даем скидку в 5%')
         break;
     case fullPrice < 15000 && fullPrice > 0:
-        console.log('Скидка не предусмотрена')
+      console.log('Скидка не предусмотрена')
         break;
     case fullPrice < 0:
         console.log('Что то пошло не так')
@@ -81,14 +113,28 @@ switch (true){
         console.log('Проверьте корректность введенных данных')
 }
 
+}
+
+ 
 
 
-getTitle()
+
+questions()
+title = getTitle()
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+servicePercentPrice = getServicePercentPrices()
+showMessage()
+
+
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf( adaptive)
 
 
 
-console.log(allServicePrices())
-console.log(fullPrice)
-console.log(servicePercentPrice())
-console.log(screens.split(''))
-
+console.log(typeof title)
+console.log(typeof screenPrice)
+console.log(typeof adaptive)
+console.log("Стоимость верстки экранов " + screenPrice + ' рублей/ долларов/гривен/юани')
+console.log('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей/ долларов/гривен/юани')
